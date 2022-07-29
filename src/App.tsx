a58d024327320a381830d7934bf39b17bbf245d6
Egie Garciano <egie.garciano@sun-asterisk.com>
2022-07-29 18:58:03 +0800

    Simple Todo App

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { useState } from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-function App() {
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from './redux/store';
+import { addTodo, removeTodo, setTodoStatus } from './redux/todoSlice';
+
+const App = () => {
+  const [todoDescription, setTodoDescription] = useState('');
+
+  const todoList = useSelector((state: RootState) => state);
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <main className='mt-11 flex h-screen flex-col items-center font-semibold'>
+      <h3 className='text mb-4 text-5xl'>Todo List</h3>
+      <form className='flex w-2/4 flex-col text-base' onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor='to-do-item'>To Do Item</label>
+        <input
+          type='text'
+          name='to-do-item'
+          id='to-do-item'
+          onChange={(e) => setTodoDescription(e.target.value)}
+          value={todoDescription}
+          className='mt-2 w-full rounded border border-slate-900 p-3 font-normal'
+        />
+        <button
+          type='submit'
+          onClick={() => {
+            todoDescription !== '' && dispatch(addTodo(todoDescription.trim()));
+            setTodoDescription('');
+          }}
+          className='mt-3 mb-4 rounded bg-blue-500 p-2 text-white'
+        >
+          Add Item
+        </button>
+      </form>
+
+      {todoList.map((todo) => (
+        <div key={todo.id} className='flex w-2/4 items-center justify-between'>
+          <div className={`${todo.completed ? 'line-through' : ''}`}>{todo.description}</div>
+          <div className='flex items-center'>
+            <button
+              data-testid='delete-button'
+              onClick={() => {
+                dispatch(removeTodo(todo.id));
+              }}
+            >
+              <DeleteIcon />
+            </button>
+            <Checkbox
+              edge='end'
+              value={todo.completed}
+              onChange={() => {
+                dispatch(setTodoStatus({ completed: !todo.completed, id: todo.id }));
+              }}
+            />
+          </div>
+        </div>
+      ))}
+    </main>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { store } from './redux/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './index.css';
 
 const container = document.getElementById('root')!;
@@ -11,10 +12,11 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
